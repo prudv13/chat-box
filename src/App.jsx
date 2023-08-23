d1844e0c1b9365998d5621a21b1ecdd8a84fdc9d
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-08-23 12:04:32 +0530

    built Login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './styles.scss';
 import Register from './pages/Register'
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <div>
-      <Register />
+      <Home />
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ChatBox from '../assets/chatbox-logo.png';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='form-container'>
+        <div className='form-wrapper'>
+        <img src={ChatBox} className='logo' />
+        <span className='title'>Login</span>
+            <form>
+                <input type='email' placeholder='Email' />
+                <input type='password' placeholder='Password' />
+                <button>Login</button>
+            </form>
+            <p>You don't have an account? Register</p>
+        </div>
+    </div>
   )
 }
 
